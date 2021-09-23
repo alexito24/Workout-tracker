51daf622e1b94640b57c6eb009abf3eefb630084
alexito24 <87586398+alexito24@users.noreply.github.com>
2021-09-22 20:50:41 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
+const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
+const routes = require("./routes/apiRoutes.js")
+const htmlRoutes = require("./routes/html.js")
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(htmlRoutes);
+app.use(routes);
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
